e96fb21ef21c36a294c8f2492a7a420838154f4c
manylovv <yellowmanylov@gmail.com>
2022-11-19 04:40:56 +0700

    fix: add forgotten proxy url

--- src/js/utils/updatePosts.js
+++ src/js/utils/updatePosts.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import _ from 'lodash';
 import parseRSS from './parseRSS';
-import { PROXY_URL } from '../constants';
+
+const proxyUrl =
+  'https://hexlet-allorigins.herokuapp.com/raw?disableCache=true&url=';
 
 const updatePosts = (state) => {
   const promises = state.feeds.map((feed) => {
     return axios
-      .get(PROXY_URL + feed.url)
+      .get(proxyUrl + feed.url)
       .then((response) => {
         const { posts } = parseRSS(response.data);
         const newPosts = _.differenceBy(posts, state.posts, 'link');
@@ -17,9 +19,7 @@ const updatePosts = (state) => {
       });
   });
 
-  Promise.all(promises).finally(() =>
-    setTimeout(updatePosts, 5000, state, PROXY_URL)
-  );
+  Promise.all(promises).finally(() => setTimeout(updatePosts, 5000, state));
 };
 
 export default updatePosts;
